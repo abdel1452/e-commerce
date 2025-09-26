509642e599cabc91686282338a5047b83f3d7d7c
abdel1452 <abdel.elidrissi25@gmail.com>
2025-09-26 09:39:17 +0200

    Applications des droits ( navbar )

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -5,7 +5,6 @@ namespace App\Controller;
 use App\Entity\User;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\UserRepository;
-use Doctrine\ORM\EntityManager;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
